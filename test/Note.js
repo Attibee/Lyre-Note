739cd702df20189375f9790b7f537cdcfea1c734
anthonymassie <ganth@DESKTOP-KR54074.PK5001Z>
2018-03-08 14:23:56 -0500

    Added transposition tests

--- test/Note.js
+++ test/Note.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var expect  = require("chai").expect;
 var Note    = require("../src/Note.js");
 
@@ -10,5 +12,29 @@ describe("Note", function() {
             
             expect(n.toString()).to.equal(note);
         }
+    });
+    
+    describe("transposes notes", function() {
+        var note = new Note('A');
+        
+        it("transposes upwards", function(){
+            expect(note.getTransposition(2).toString()).to.equal('B');
+        });
+        
+        it("transposes upwards over octaves", function(){
+            expect(note.getTransposition(26).toString()).to.equal('B');
+        });
+        
+        it("transposes downwards", function(){
+            expect(note.getTransposition(-2).toString()).to.equal('G');
+        });
+        
+        it("transposes downwards over octaves", function(){
+            expect(note.getTransposition(-26).toString()).to.equal('G');
+        });
+        
+        it("transposes self", function() {
+            expect(note.transpose(26).toString()).to.equal('B');
+        });
     });
 });
