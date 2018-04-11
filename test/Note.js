433c4ee8c5227b88e30a56f5f7a69866845f698b
anthonymassie <ganth@DESKTOP-KR54074.PK5001Z>
2018-04-10 23:20:08 -0400

    Updated tests

--- test/Note.js
+++ test/Note.js
@@ -5,14 +5,14 @@ var Note    = require("../src/Note.js");
 var InvalidNote = require("../index.js").InvalidNote;
 
 describe("Note", function() {
-    /*describe("set notes", function() {
-        it("creates all notes", function() {
+    describe("set notes", function() {
+        it("sets all notes", function() {
             var notes = ['Ab', 'A', 'A#', 'Bb', 'B', 'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'F', 'F#', 'Gb', 'G', 'G#'];
 
             for(let note of notes) {
                 var n = new Note(note);
 
-                expect(n.toString()).to.equal(note);
+                expect(n.getNote()).to.equal(note);
             }
         });
         
@@ -26,23 +26,23 @@ describe("Note", function() {
         var note = new Note('A');
         
         it("transposes upwards", function(){
-            expect(note.getTransposition(2).toString()).to.equal('B');
+            expect(note.getTransposition(2).getNote()).to.equal('B');
         });
         
         it("transposes upwards over octaves", function(){
-            expect(note.getTransposition(26).toString()).to.equal('B');
+            expect(note.getTransposition(26).getNote()).to.equal('B');
         });
         
         it("transposes downwards", function(){
-            expect(note.getTransposition(-2).toString()).to.equal('G');
+            expect(note.getTransposition(-2).getNote()).to.equal('G');
         });
         
         it("transposes downwards over octaves", function(){
-            expect(note.getTransposition(-26).toString()).to.equal('G');
+            expect(note.getTransposition(-26).getNote()).to.equal('G');
         });
         
         it("transposes self", function() {
-            expect(note.transpose(26).toString()).to.equal('B');
+            expect(note.transpose(26).getNote()).to.equal('B');
         });
-    });*/
+    });
 });
--- test/Stringify.js
+++ test/Stringify.js
@@ -1,35 +1,30 @@
 'use strict';
 
 var expect = require("chai").expect;
-var Stringify = require("../src/Stringify.js");
-var Note = require("../src/Note.js");
+var Note = require("../index.js");
+var Stringify = Note.Stringify;
+var Parse = Note.Parse;
 
 describe("Stringify", function() {
-   /* describe("stringifies notes", function() {
-        var map = {
-            "C0": "C,,",
-            "C1": "C,",
-            "C2": "C",
-            "C3": "c",
-            "C4": "c'",
-            "C5": "c''"
-        };
-        
-        it("stringifies all notes to helmholtz", function() {
-            for(let scientific in map) {
-                var helmholtz = map[scientific];
-                var string = Stringify(new Note(scientific), Stringify.HELMHOLTZ);
-
-                expect(string).to.equal(helmholtz);
-            }
+    describe("stringifies helmholtz", function() {
+        it("stringifies all octaves", function() {
+            expect( Stringify( new Note('C', 0), Stringify.HELMHOLTZ ) ).to.equal("C,,");
+            expect( Stringify( new Note('C', 1), Stringify.HELMHOLTZ ) ).to.equal("C,");
+            expect( Stringify( new Note('C', 2), Stringify.HELMHOLTZ ) ).to.equal("C");
+            expect( Stringify( new Note('C', 3), Stringify.HELMHOLTZ ) ).to.equal("c");
+            expect( Stringify( new Note('C', 4), Stringify.HELMHOLTZ ) ).to.equal("c'");
+            expect( Stringify( new Note('C', 5), Stringify.HELMHOLTZ ) ).to.equal("c''");
+        });
+    });
+    
+    describe("stringifies scientific", function() {
+        it("stringifies basic notes", function() {
+            expect( Stringify( new Note('C', 0) ) ).to.equal("C0");
+            expect( Stringify( new Note('C', 1) ) ).to.equal("C1");
         });
         
-        it("stringifies all notes to scientific", function() {
-            for(let scientific in map) {
-                var string = Stringify(new Note(scientific), Stringify.SCIENTIFIC);
-
-                expect(string).to.equal(scientific);
-            }
+        it("stringify scientific without octave", function() { 
+            expect(Stringify(new Note("C"))).to.equal("C");
         });
-    });*/
+    });
 });
