d84f091d7555ba69111c13badbe5ef473fef1540
anthonymassie <ganth@DESKTOP-KR54074.PK5001Z>
2018-04-10 22:52:47 -0400

    Updating exports

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 
 module.exports = require("./src/Note.js");
 module.exports.InvalidNote = require("./src/Exception/InvalidNote.js");
+module.exports.Parse = require("./src/Parse.js");
+module.exports.Stringify = require("./src/Stringify.js");
